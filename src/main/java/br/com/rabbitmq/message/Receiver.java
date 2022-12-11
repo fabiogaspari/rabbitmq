e57f31bf2d05820ade7a2c12fd8f4bfeae5723f3
Fabio <fabio.gaspari@gmail.com>
2022-12-11 00:14:45 -0300

    removid unnecessary code

--- src/main/java/br/com/rabbitmq/message/Receiver.java
+++ src/main/java/br/com/rabbitmq/message/Receiver.java
@@ -1,28 +1,14 @@
 package br.com.rabbitmq.message;
 
-import java.util.concurrent.CountDownLatch;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
 
-  private CountDownLatch latch = new CountDownLatch(1);
-
-  Logger logger = LoggerFactory.getLogger(Receiver.class);
-
   @RabbitListener(queues = "laravel-spring")
   public void receiveMessage(String message) throws InterruptedException {
-    logger.info(message.toString());
     System.out.println("Received <" + message + ">");
-    latch.countDown();
-  }
-
-  public CountDownLatch getLatch() {
-    return latch;
   }
 
 }
